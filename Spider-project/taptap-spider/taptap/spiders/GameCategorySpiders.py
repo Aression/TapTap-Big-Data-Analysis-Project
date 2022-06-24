3484913067fd07bc43be473dff6eb6af8d0c8916
Aression <lvdifine@gmail.com>
2022-06-24 11:30:18 +0800

    修复评论拉取数量不对应的问题；增加调度器和日志管理

--- Spider-project/taptap-spider/taptap/spiders/GameCategorySpiders.py
+++ Spider-project/taptap-spider/taptap/spiders/GameCategorySpiders.py
@@ -3,7 +3,6 @@
 
 import logging
 import json
-import jsonlines
 import pypinyin
 import scrapy
 from scrapy import FormRequest
@@ -73,6 +72,7 @@ def _category_details_spider_factory(_name: str = 'CategoryDetailsSpider_name_UN
                 item['downloads'] = db['data']['stat']['play_total']
                 item['vote_info'] = db['data']['stat']['vote_info']
 
+                item['comment'] = []
                 # crawl comments
                 yield FormRequest(
                     url=domain + app_reviews.format(
@@ -91,9 +91,9 @@ def _category_details_spider_factory(_name: str = 'CategoryDetailsSpider_name_UN
                 db = json.loads(response.text)
                 item = response.meta['item']
                 size = response.meta['size'] + len(db['data']['list'])
-                item['comment'] = [
+                item['comment'].extend([
                     i['moment']['extended_entities']['reviews'][0]['contents']['text'] for i in db['data']['list']
-                ]
+                ])
                 yield item
                 if db['data']['next_page'] != '' and size < 100:
                     yield FormRequest(
