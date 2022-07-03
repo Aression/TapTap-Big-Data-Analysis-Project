8a5aae8eb5873d125f3541ef6ab35b0dcdfd1193
Aression <1040901628@qq.com>
2022-07-03 09:32:05 +0800

    Update api.js

--- Frontend-project/src/http/api.js
+++ Frontend-project/src/http/api.js
@@ -1,9 +1,10 @@
 import axios from './axios.js'
+let domain="http://39.108.88.241:8003"
 let url = {
     // 获取榜单
     getHotTable(data) {
         return axios({
-            url: "https://mockapi.eolink.com/F2DDYfm2cb2a6dfd5fc95bdcc9c6f68fb74286b837564e4/BasicData/getHotTable?list_name="+data.listname,
+            url: domain+"/BasicData/getHotTable?list_name="+data.listname,
             method: "post",
             data
         })
@@ -11,7 +12,7 @@ let url = {
     // 获取榜单统计图表
     getHotTableChart(data) {
         return axios({
-            url: "https://mockapi.eolink.com/F2DDYfm2cb2a6dfd5fc95bdcc9c6f68fb74286b837564e4/BasicData/getHotTableChart?list_name="+data.listname,
+            url: domain+"/BasicData/getHotTableChart?list_name="+data.listname,
             method: "post",
             data
         })
@@ -19,7 +20,7 @@ let url = {
     // 获取榜单(综合关注度 或者 叫座榜)
     getTabs_ac(data) {
         return axios({
-            url: "https://mockapi.eolink.com/F2DDYfm2cb2a6dfd5fc95bdcc9c6f68fb74286b837564e4/Comprehensive/GetTableAC?list_name="+data.list_name,
+            url: domain+"/Comprehensive/GetTableAC?list_name="+data.list_name,
             method: "post",
             data
         })
@@ -28,7 +29,7 @@ let url = {
      // 游戏类型  评分和下载量
      get_gametype(data) {
         return axios({
-            url: "https://mockapi.eolink.com/F2DDYfm2cb2a6dfd5fc95bdcc9c6f68fb74286b837564e4/Comprehensive/GameTypeAnalysis?gametype_list="+data.gametype_list,
+            url: domain+"/Comprehensive/GameTypeAnalysis?gametype_list="+data.gametype_list,
             method: "post",
             data
         })
@@ -37,7 +38,7 @@ let url = {
     //厂商评分分析
     get_manu(data) {
         return axios({
-            url: "https://mockapi.eolink.com/F2DDYfm2cb2a6dfd5fc95bdcc9c6f68fb74286b837564e4/Comprehensive/ManuScore?manufacturer="+data.manufacturer,
+            url: domain+"/Comprehensive/ManuScore?manufacturer="+data.manufacturer,
             method: "post",
             data
         })
@@ -45,7 +46,7 @@ let url = {
      //搜索榜单
      getcurveTabs(data) {
         return axios({
-            url: "https://mockapi.eolink.com/F2DDYfm2cb2a6dfd5fc95bdcc9c6f68fb74286b837564e4/search-page?search_game-name=" + data.search_name,
+            url: domain+"/search-page?search_game-name=" + data.search_name,
             method: "post",
             data
         })
@@ -53,7 +54,7 @@ let url = {
     //曲线数据
     getcurve(data) {
         return axios({
-            url: "https://mockapi.eolink.com/F2DDYfm2cb2a6dfd5fc95bdcc9c6f68fb74286b837564e4/data",
+            url: domain+"/data",
             method: "post",
             data
         })
