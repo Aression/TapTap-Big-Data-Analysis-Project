03c3bf394b62cbf034c7cbcaf5a3a6895a1d2b71
HLJ <HuangLJ0408@outlook.com>
2022-07-03 17:08:13 +0800

    厂商榜

--- Frontend-project/src/components/Basic data/Hot.vue
+++ Frontend-project/src/components/Basic data/Hot.vue
@@ -1,17 +1,29 @@
 <template>
     <div>
         <div style="width: 700px;margin: 0 auto;">
-            <el-table :data="tableData" style="width: 100%;" height="400">
-                <el-table-column prop="name" label="游戏名称" width="250" align="center">
-                </el-table-column>
-                <el-table-column prop="score" label="评分" width="200" align="center">
-                </el-table-column>
-                <el-table-column prop="type" label="游戏类型" width="250" align="center">
-                    <template slot="header" slot-scope="scope">
-                        游戏类型
-                        <i class="el-icon-s-data" @click="toChart"></i>
-                    </template>
-                </el-table-column>
+            <el-table :data="tableData" style="width: 100%;" height="400" >
+                <template  v-if="chartTitle!=='厂商榜'">
+                    <el-table-column prop="name" label="游戏名称" width="250" align="center">
+                    </el-table-column>
+                    <el-table-column prop="score" label="评分" width="200" align="center">
+                    </el-table-column>
+                    <el-table-column prop="type" label="游戏类型" width="250" align="center">
+                        <template slot="header" slot-scope="scope">
+                            游戏类型
+                            <i class="el-icon-s-data" @click="toChart"></i>
+                        </template>
+                    </el-table-column>
+                </template >
+                <template  v-if="chartTitle=='厂商榜'">
+                    <el-table-column prop="name" label="厂商名称" width="250" align="center">
+                    </el-table-column>
+                    <el-table-column prop="type" label="总下载量" width="250" align="center">
+                        <template slot="header" slot-scope="scope">
+                            总下载量
+                            <i class="el-icon-s-data" @click="toChart"></i>
+                        </template>
+                    </el-table-column>
+                </template >
             </el-table>
         </div>
     </div>
