efd21fa5892cdfe8bf9d4ab8cdfa12e347a15c95
ququ-123 <67093491+ququ-123@users.noreply.github.com>
2022-07-03 09:43:47 +0800

    Update BaseDataResponsor.py

--- Backend-project/Responsor/BaseDataResponsor.py
+++ Backend-project/Responsor/BaseDataResponsor.py
@@ -1,6 +1,64 @@
 from .ResponsorHeader import *
 
 route_basicdata = Blueprint('BasicData', __name__)
+# 获取对应榜单的游戏类型数据,返回字典，比如{'MMORPG': 1, '二次元': 1, '冒险': 1, '魔幻': 1, '3D ': 1}
+def getHotTableChart(list_name):
+    if (list_name == "heat_rank"):
+        heat_games = game_list.query.filter(game_list.heat_rank != 0).all()  # 取出热榜游戏
+        cateList = []
+        for heat_game in heat_games:
+            heat_game_cateList = str(heat_game.cates).split(',')
+            for i in heat_game_cateList:
+                cateList.append(str(i))
+        cate_count = {}
+        for item in cateList:
+            if item in cate_count:
+                cate_count[item] += 1
+            else:
+                cate_count[item] = 1
+        return cate_count
+    if (list_name == "played_rank"):
+        played_games = game_list.query.filter(game_list.played_rank != 0).all()  # 取出热榜游戏
+        cateList = []
+        for played_game in played_games:
+            played_game_cateList = str(played_game.cates).split(',')
+            for i in played_game_cateList:
+                cateList.append(str(i))
+        cate_count = {}
+        for item in cateList:
+            if item in cate_count:
+                cate_count[item] += 1
+            else:
+                cate_count[item] = 1
+        return cate_count
+    if (list_name == "reserved_rank"):
+        reserved_games = game_list.query.filter(game_list.reserved_rank != 0).all()  # 取出热榜游戏
+        cateList = []
+        for reserved_game in reserved_games:
+            reserved_game_cateList = str(reserved_game.cates).split(',')
+            for i in reserved_game_cateList:
+                cateList.append(str(i))
+        cate_count = {}
+        for item in cateList:
+            if item in cate_count:
+                cate_count[item] += 1
+            else:
+                cate_count[item] = 1
+        return cate_count
+    if (list_name == "sold_rank"):
+        sold_games = game_list.query.filter(game_list.sold_rank != 0).all()  # 取出热榜游戏
+        cateList = []
+        for sold_game in sold_games:
+            sold_game_cateList = str(sold_game.cates).split(',')
+            for i in sold_game_cateList:
+                cateList.append(str(i))
+        cate_count = {}
+        for item in cateList:
+            if item in cate_count:
+                cate_count[item] += 1
+            else:
+                cate_count[item] = 1
+        return cate_count
 
 #Unfinished------------------------------------------------------------------
 @route_basicdata.route('/getHotTableChart', methods=["GET","POST"])
