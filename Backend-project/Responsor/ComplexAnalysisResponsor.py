da0387e28ebc81b8a410abc6bb0c1837218a17c8
ART0189 <1264931036@qq.com>
2022-07-03 16:08:39 +0800

    Update ComplexAnalysisResponsor.py

--- Backend-project/Responsor/ComplexAnalysisResponsor.py
+++ Backend-project/Responsor/ComplexAnalysisResponsor.py
@@ -7,12 +7,18 @@ route_complex = Blueprint('Comprehensive', __name__)
 def ComprehensiveAnalysis():
     req=request.values
     reqlist=req['manufacturer']
-    resp = {'code': 200, 'status': 'failed','error_msg':'','data_list':[]}
+    resp = {'code': 200, 'status': 'success','error_msg':'','data_list':[]}
     
-    Comp=company_list.query.filter_by(company_name=reqlist).first()
-    if Comp!=null:
-        resp['data_list'][0]={'manu_name':reqlist,'manu_score':[Comp.one_star,Comp.two_star,Comp.three_star, \
-            Comp.four_star,Comp.five_star]}
+    NowCnt=0
+    MaxCnt=100
+    Comps=company_list.query.all()
+    if Comps!=null:
+        for Comp in Comps:
+            if NowCnt>=MaxCnt:
+                break
+            NowCnt+=1
+            resp['data_list'].append({'manu_name':reqlist,'manu_score':[Comp.one_star,Comp.two_star,Comp.three_star, \
+                Comp.four_star,Comp.five_star]})
     else:
         resp['error_msg']='Invalid CompName'
 
@@ -22,11 +28,11 @@ def ComprehensiveAnalysis():
 def TypeAnalysis():
     req=request.values
     reqlist=req['gametype_list']
-    resp = {'code': 200, 'status': 'failed','error_msg':'','data_list':[]}
+    resp = {'code': 200, 'status': 'success','error_msg':'','data_list':[]}
 
-    if reqlist=="游戏类型评分分析":
+    if reqlist=="游戏类型评分分析" or reqlist=="游戏类型下载量分析":
         BaseDict={}
-        Cates=cate_list.query_all()
+        Cates=cate_list.query.all()
         for i in Cates:
             BaseDict['game_typename']=i.cate_name
             BaseDict['download_list']=i.downlo
@@ -42,7 +48,7 @@ def TypeAnalysis():
 def CompanyAnalysis():
     req=request.values
     reqlist=req['list_name']
-    resp = {'code': 200, 'status': 'failed','error_msg':'','list_info':[]}
+    resp = {'code': 200, 'status': 'success','error_msg':'','list_info':[]}
 
     dates = [
         datetime.date(2022,6,i) for i in range(22,30)
@@ -51,9 +57,7 @@ def CompanyAnalysis():
     a = [random.randint(a_center-i,a_center+i) for i in range(8)]
     c_center = 40
     c = [random.randint(c_center-i,c_center+i) for i in range(8)]
-    if reqlist=="综合关注度榜":
-        resp['list_info'].append({'date':dates,'data_a':a,'data_c':c})
-    elif reqlist=="叫座榜":
+    if reqlist=="叫座榜与综合关注度榜":
         resp['list_info'].append({'date':dates,'data_a':a,'data_c':c})
     else:
         resp['error_msg']='Invalid ListName'
