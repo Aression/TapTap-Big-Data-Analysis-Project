e61732f50d061bbd93b623e211f13d1ec69cfb3f
ART0189 <1264931036@qq.com>
2022-07-03 18:08:47 +0800

    Release Frontend

--- Frontend-project/src/components/Comprehensive analysis/Tab.vue
+++ Frontend-project/src/components/Comprehensive analysis/Tab.vue
@@ -1,24 +1,38 @@
 <template>
     <div>
         <div class="tab">
-            <div style="width: 750px;">
+            <div style="width: 750px; margin: auto; ">
                 <el-table :data="tableData" style="width: 100%;" height="360">
-                    
-                    <el-table-column prop="type" :label="nowIndex == 3 ? '游戏厂商/星级' : '游戏类型/星级'" width="140"
-                        align="center">
-                    </el-table-column>
-                    <el-table-column prop="one" label="一星" width="100" align="center">
-                    </el-table-column>
-                    <el-table-column prop="two" label="二星" width="100" align="center">
-                    </el-table-column>
-                    <el-table-column prop="three" label="三星" width="100" align="center">
-                    </el-table-column>
-                    <el-table-column prop="four" label="四星" width="100" align="center">
-                    </el-table-column>
-                    <el-table-column prop="five" label="五星" width="100" align="center">
-                    </el-table-column>
-                    <el-table-column prop="total" label="总数" width="100" align="center">
-                    </el-table-column>
+                    <template v-if="nowIndex === 3">
+                        <el-table-column prop="type" :label="nowIndex == 3 ? '游戏厂商/星级' : '游戏类型/星级'" width="140"
+                            align="center">
+                        </el-table-column>
+                        <el-table-column prop="one" label="一星" width="100" align="center">
+                        </el-table-column>
+                        <el-table-column prop="two" label="二星" width="100" align="center">
+                        </el-table-column>
+                        <el-table-column prop="three" label="三星" width="100" align="center">
+                        </el-table-column>
+                        <el-table-column prop="four" label="四星" width="100" align="center">
+                        </el-table-column>
+                        <el-table-column prop="five" label="五星" width="100" align="center">
+                        </el-table-column>
+                        <el-table-column prop="total" label="总数" width="100" align="center">
+                        </el-table-column>
+                    </template>
+                    <template v-if="nowIndex == 2">
+                        <el-table-column prop="type" label="游戏类型" width="350" align="center">
+                        </el-table-column>
+                        <el-table-column prop="list" label="总下载量" width="350" align="center">
+                        </el-table-column>
+                    </template>
+                    <template v-if="nowIndex == 1">
+                        <el-table-column prop="type" label="游戏类型" width="350" align="center">
+                        </el-table-column>
+                        <el-table-column prop="list" label="评论总数" width="350" align="center">
+                        </el-table-column>
+                    </template>
+
                     <!-- <el-table-column v-for="{ type, one, two, three, four, five, total } in tableData" :key="type"
                         :one="one" :two="two" :three="three" :four="four" :five="five" :total="total">
                     </el-table-column> -->
@@ -78,13 +92,18 @@ export default {
             s += this.data_GameType[this.data_GameType.length - 1].list[index1++] + '"';
             s += '}'
             s += ']'
-            // console.log(JSON.parse(s))
+            console.log(JSON.parse(s))
             this.tableData = JSON.parse(s)
         },
     },
     mounted() {
-        this.changeform()
-        // console.log(this.data_GameType)
+        if (this.nowIndex == 3) {
+            this.changeform()
+        }
+        else {
+            this.tableData = this.data_GameType
+        }
+        console.log(this.data_GameType)
         //this.changeform_manu()
     }
 }
@@ -96,7 +115,8 @@ export default {
     width: 100%;
     height: 100%;
     /* margin: 0 auto; */
-    margin: 0 auto; text-align: center;
+    margin: 0 auto;
+    text-align: center;
     display: flex;
     justify-content: center;
     align-content: center;
